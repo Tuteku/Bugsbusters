57c4bb05f8b35bffb29ddea13f15d0a7ecc6dd9b
Jiaku21 <ruvca2000@outlook.com>
2025-10-07 17:55:39 -0300

    Cambios al Hero Banner y responsive
    Centre el  texto y boton del hero banner y agregue que se oculte el header al scrollear en mobile

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar({ carrito }) {
     // Mostrar el total de unidades en el carrito (sumatoria de `cantidad`)
     const cantidadCarrito = carrito ? carrito.reduce((sum, p) => sum + (p.cantidad || 0), 0) : 0;
+    
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.pageYOffset;
+            const isVisible = prevScrollPos > currentScrollPos || currentScrollPos < 10;
+
+            setPrevScrollPos(currentScrollPos);
+            setVisible(isVisible);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [prevScrollPos]);
 
   return (
-    <header>
+    <header className={visible ? '' : 'header-hidden'}>
         <div className="navbar">
             <Link to="/"><img src={`${process.env.PUBLIC_URL}/assets/logo.svg`} alt="Logo de la Mueblería Hermanos Jota" />
 </Link>
